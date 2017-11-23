afa174185e0a007ec522f586053d7114ff997623
robot_rover <sam.obrien00@gmail.com>
2017-11-23 01:28:50 -0600

    Made port customizable

--- src/main/java/procul/studios/ProcelioServer.java
+++ src/main/java/procul/studios/ProcelioServer.java
@@ -9,22 +9,24 @@ public class ProcelioServer {
     public static String databasePath;
     public static String sslKeystore;
     public static String keystorePass;
+    public static int port;
     public static boolean useSsl = false;
     public ProcelioServer(){
 
     }
     public static void main(String[] args){
-        if(args.length < 1)
-            throw new RuntimeException("Requires 1 argument");
+        if(args.length < 2)
+            throw new RuntimeException("Requires 2 arguments");
         databasePath = args[0];
-        if(args.length >= 3){
-            sslKeystore = args[1];
-            keystorePass = args[2];
+        port = Integer.parseInt(args[1]);
+        if(args.length >= 4){
+            sslKeystore = args[2];
+            keystorePass = args[3];
             useSsl = true;
         }
         Database database = new Database();
         DatabaseWrapper wrapper = new DatabaseWrapper(database.getContext());
-        SparkServer server = new SparkServer(100, wrapper);
+        SparkServer server = new SparkServer(port, wrapper);
         server.start();
     }
 }
