88264519c57f65d994f4d7902c6f6875586e5a06
Brennan Stein <brenn22@cs.washington.edu>
2019-12-29 10:50:45 -0800

    indefinite login session option

--- ProcelioServer/src/main/java/procul/studios/ClientEndpoints.java
+++ ProcelioServer/src/main/java/procul/studios/ClientEndpoints.java
@@ -437,7 +437,7 @@ public class ClientEndpoints {
             String ip = req.headers("CF-Connecting-IP");
             if(ip == null)
                 ip = req.ip();
-            long expires = Instant.now().plus(1, ChronoUnit.DAYS).getEpochSecond();
+            long expires = auth.noexpire ? Long.MAX_VALUE : Instant.now().plus(1, ChronoUnit.DAYS).getEpochSecond();
             context.deleteFrom(AUTHTABLE).where(AUTHTABLE.USERID.eq(id)).execute();
             context.insertInto(AUTHTABLE).set(AUTHTABLE.USERID, databaseHash.component2()).set(AUTHTABLE.CLIENTIP, ip).set(AUTHTABLE.EXPIRES, expires).set(AUTHTABLE.TOKEN, token).execute();
             context.update(USERTABLE).set(USERTABLE.LASTLOGIN, Instant.now().getEpochSecond()).where(USERTABLE.ID.eq(id)).execute();
